838dde30bf4c0a26852035ee93061e8130b4f08b
Hede <Heatherj.swift@gmail.com>
2021-05-25 19:01:51 -0400

    initiate database in schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,23 @@
+INSERT INTO department (name)
+VALUES
+    ('Sales'),    
+    ('Engineering'), 
+    ('Finance'), 
+    ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Sales person', 80000, 1),
+    ('Lead engineer', 150000, 2),
+    ('Software engineer', 120000, 2),
+    ('Accountant', 125000, 3),
+    ('Legal team lead', 250000, 4),
+    ('Lawyer', 250000, 4);
+
+-- UPDATE employee SET manager_id = 9 WHERE id in (10,11); ???
+-- UPDATE employee SET manager_id = 12 where id in (13,14); ???
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, 3),
@@ -10,19 +30,6 @@ VALUES
     ('Tammer', 'Galal', 4, 3);
 
 
-INSERT INTO role (title, salary, department_id)
-VALUES
-    ('Sales Lead', 100000, 1),
-    ('Sales person', 80 000, 1),
-    ('Lead engineer', 150000, 2),
-    ('Software engineer', 120000, 2),
-    ('Accountant', 125000, 3),
-    ('Legal team lead', 250000, 4),
-    ('Lawyer', 250000, 4);
 
-INSERT INTO department (name)
-VALUES
-    ('Sales'),    
-    ('Engineering'), 
-    ('Finance'), 
-    ('Legal');
+
+
