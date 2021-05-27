a305f8dd49dcce4b7237505fc47b12508988e05c
Hede <Heatherj.swift@gmail.com>
2021-05-26 22:57:26 -0400

    add view mysql sequences and add mysql sequences

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,35 +1,34 @@
-INSERT INTO department (name)
+INSERT INTO department (id, name)
 VALUES
-    ('Sales'),    
-    ('Engineering'), 
-    ('Finance'), 
-    ('Legal');
+    (1, 'Sales'),    
+    (2, 'Engineering'), 
+    (3, 'Finance'), 
+    (4, 'Legal');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role (id, title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Sales person', 80000, 1),
-    ('Lead engineer', 150000, 2),
-    ('Software engineer', 120000, 2),
-    ('Accountant', 125000, 3),
-    ('Legal team lead', 250000, 4),
-    ('Lawyer', 250000, 4);
+    (1, 'Sales Lead', 100000, 1),
+    (2, 'Sales person', 80000, 1),
+    (3, 'Lead engineer', 150000, 2),
+    (4, 'Software engineer', 120000, 2),
+    (5, 'Accountant', 125000, 3),
+    (6, 'Legal team lead', 250000, 4),
+    (7, 'Lawyer', 250000, 4);
 
--- UPDATE employee SET manager_id = 9 WHERE id in (10,11); ???
--- UPDATE employee SET manager_id = 12 where id in (13,14); ???
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id)
 VALUES
-    ('John', 'Doe', 1, 3),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriquez', 3),
-    ('Kevin', 'Tupile', 4, 3),
-    ('Malia', 'Brown', 5),
-    ('Sarah', 'Lourd', 6),
-    ('Tom', 'Allen', 7, 6),
-    ('Tammer', 'Galal', 4, 3);
+    (1, 'John', 'Doe', 1),
+    (2, 'Mike', 'Chan', 2),
+    (3, 'Ashley', 'Rodriquez', 3),
+    (4, 'Kevin', 'Tupile', 4),
+    (5, 'Malia', 'Brown', 5),
+    (6, 'Sarah', 'Lourd', 6),
+    (7, 'Tom', 'Allen', 7),
+    (8, 'Tammer', 'Galal', 4);
 
 
-
+UPDATE employee SET manager_id = 3 WHERE id in (1,4,8); 
+UPDATE employee SET manager_id = 1 WHERE id in (2);
+UPDATE employee SET manager_id = 6 WHERE id in (7);
 
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const db = require('./db/connection');
-const consoleTable = require('console.table');
+//const consoleTable = require('console.table');
 
 
 const mainMenu = async () => {
@@ -50,10 +50,6 @@ const mainMenu = async () => {
 };
 
 function viewDepartments() {
-//   const sql = `SELECT department.* 
-//                 FROM department`;
-
-
   db.promise().query(`SELECT department.* FROM department`)    
     .then(([ rows ]) => {
         console.table(rows)
@@ -63,117 +59,81 @@ function viewDepartments() {
       console.log(err);
     }); 
   };
-//   );
-// }
 
-// function viewRoles() {
-//   const sql = `SELECT role.*
-//                 FROM role
-//                 ON role.department_id = department.id`;
-//   db.query(sql, (err, resolve, reject) => {
-//     if (err) {
-//       reject(err);
-//       return;
-//     }
+function viewRoles() {
+  db.promise().query(`SELECT role.*, department.name AS department_name FROM role LEFT JOIN department ON role.department_id = department.id`)    
+    .then(([ rows ]) => {
+        console.table(rows)
+        mainMenu();
+    })
+    .catch(err=> {
+      console.log(err);
+    }); 
+  };
 
-//     resolve({
-//       ok: true,
-//       message: "success",
-//     });
-//   });
-// }
+function viewEmployees() {
+  db.promise().query(`SELECT employee.*, role.title, role.salary, department.name 
+                      FROM employee  
+                      LEFT JOIN role 
+                      ON employee.role_id = role.id 
+                      LEFT JOIN department 
+                      ON employee.department_id = department.id`)    
+    .then(([ rows ]) => {
+        console.table(rows)
+        mainMenu();
+    })
+    .catch(err=> {
+      console.log(err);
+    }); 
+  };
 
-// function viewEmployees() {
-//   const sql = `SELECT employee.*, role.title, role.salary, department.name
-//                 FROM employee
-//                 LEFT JOIN role
-//                 ON employee.role_id = role.id
-//                 LEFT JOIN department
-//                 ON employee.department_id = department.id`;
+function addDepartment() {
+  return inquirer
+    .prompt([
+      {
+        type: "text",
+        name: "department",
+        message: "What is the name of the department?",
+      },
+    ])
+    .then((response) => {
+      const res = response.department;
+      db.promise()
+        .query(`INSERT INTO department (name) VALUES (?)`, [res])
+        .then(([rows]) => {
+          console.log(`added ${res} department`);
+          mainMenu();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
 
-//   db.query(sql, (err, resolve, reject) => {
-//     if (err) {
-//       reject(err);
-//       return;
-//     }
-
-//     resolve({
-//       ok: true,
-//       message: "success",
-//     });
-//   });
-// }
-
-// function addDepartment() {
-//   return inquirer
-//     .prompt([
-//       {
-//         type: "text",
-//         name: "department",
-//         message: "What is the name of the department?",
-//       },
-//     ])
-//     .then((response) => {
-//       const res = response.department;
-//       db.promise()
-//         .query(`INSERT INTO department (name) VALUES (?)`, [res])
-//         .then(([rows]) => {
-//           console.log(`added ${res} department`);
-//           mainMenu();
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     });
-// }
+function addRole() {
+  return inquirer
+    .prompt([
+      {
+        type: "text",
+        name: "role",
+        message: "What is the title of the role?",
+      },
+    ])
+    .then((response) => {
+      const res = response.role;
+      db.promise()
+        .query(`INSERT INTO role (title) VALUES (?)`, [res])
+        .then(([rows]) => {
+          console.log(`added ${res} role`);
+          mainMenu();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
 
 mainMenu();
-// //function addRole
-
-// // const promptUser = () => {
-// //     return inquirer.prompt([
-// //         {
-// //             type: 'rawlist',
-// //             name: 'startMenu',
-// //             message: 'What would you like to do?',
-// //             choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
-// //         }
-// //     ])
-// // };
-
-// // prompts.next({})
-
-// //add department
-// // const sql = `INSERT INTO department (name)
-// //              VALUES (?)`;
-
-// // db.query(sql, (err, resolve, reject) => {
-// //     if (err) {
-// //         reject(err);
-// //         return;
-// //     }
-
-// //     resolve({
-// //         ok: true,
-// //         message: "department added successfully"
-// //     });
-// // })
-
-// //add role
-// // const sql = `INSERT INTO role (title, salary)
-// //              VALUES (?,?)`;
-
-// // db.query(sql, (err, resolve, reject) => {
-// //     if (err) {
-// //         reject(err);
-// //         return;
-// //     }
-
-// //     resolve({
-// //         ok: true,
-// //         message: "role added successfully"
-// //     });
-// // })
 
 // // add employee
 // // const sql = `INSERT INTO employee (first_name, last_name, manager)
